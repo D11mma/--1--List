4a0bc6e4ba5b5065ab4119f26f4c1e429f4251d9
admin <admin@DESKTOP-A9TRHHS>
2024-03-05 19:42:11 +0200

    Добавьте файлы проекта.

---
+++ списки List/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program 
+{  
+    static void Main()
+    {
+        List<int> list = new List<int>();
+        Console.Write("Скільки чисел додати в список:");
+        int n = int.Parse(Console.ReadLine());
+
+        int k;
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Введіть число:");
+            k = int.Parse(Console.ReadLine());
+            list.Add(k);
+        }
+        Console.Write("Список створено:");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i] + " ");
+        }
+
+        Console.Write("Введіть індекс числа, яке бажаєте видалити:");
+        k = int.Parse(Console.ReadLine());
+
+        if (k >= 0 && k < n)
+        {
+            list.RemoveAt(k); // приймає номер позиції   
+        }
+        else
+        {
+            Console.Write("такої позиції не існує");
+        }
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
